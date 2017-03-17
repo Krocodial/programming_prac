fee5f34232002cb333ca034261bc6969342796a1
Krocodial <xxkraaklxx@gmail.com>
2017-03-16 22:50:30 -0700

    finished bubble sort

--- python/bubble.py
+++ python/bubble.py
@@ -1,11 +1,35 @@
 #!/usr/bin/python3.5
 
+#Takes in data in one value per line from text file and sorts it
+
 import sys
 
-def sort(filename):
-	f = open(filename, 'r')
+try:
+	f = open(sys.argv[1], 'r')
+except IOError:
+	print("There was an error reading from the file")
+	sys.exit()
+numbers = []
+for line in f:
+	numbers.append(int(line))
 
+def switch(a, b):
+	c = tmp[a]
+	tmp[a] = tmp[b]
+	tmp[b] = c
 
-for i in range(50):
-	f.write(str(random.randrange(30)) + '\n')
+tmp = numbers
 
+print(numbers)
+print('++++++++++++++++')
+
+for i in numbers:
+	for j in range(len(numbers)-1):
+		if(numbers[j] < numbers[j+1]):
+			print(str(numbers[j]) + ', ' + str(numbers[j+1]))
+			switch(j, j+1)
+	numbers = tmp
+
+print(numbers)
+
+f.close()
