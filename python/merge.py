be0695c44bf957f8ca863d213d71ab0a6b3a746e
Krocodial <xxkraaklxx@gmail.com>
2017-03-19 17:19:36 -0700

    finished merge_sort

--- python/merge.py
+++ python/merge.py
@@ -17,7 +17,6 @@ array = []
 for line in f:
 	array.append(int(line))
 
-
 #Now we merge sort our array
 
 #first we need our recursive function
@@ -26,23 +25,51 @@ print('original')
 print(array)
 
 
-def merge_together(start, middle, end):
-	#need to iterate through all elements and sort in place
-	pass
-
-
-def merge_sort(start, end):
-	if((end-start) < 2):
+def merge_sort(array):
+	if(len(array) < 2):
 		return
 	#otherwise we have more than 2 elements, so break it in half
-	middle = (start + end)/2
+	middle = len(array)//2
 	
-	merge_sort(start, middle)
-	merge_sort(middle+1, end)
+	lefthalf = array[:middle]
+	righthalf = array[middle:]
+
+	merge_sort(lefthalf)
+	merge_sort(righthalf)
+	
+	#We are left with 2 arrays of varying length that are sorted
+	i = 0
+	j = 0
+	k = 0
+
+	while(i != len(lefthalf) and j != len(righthalf)):
+		if(lefthalf[i] > righthalf[j]):
+			array[k] = lefthalf[i]
+			i += 1
+		else:
+			array[k] = righthalf[j]
+			j += 1
+		k+=1
+	while(i != len(lefthalf)):
+		array[k] = lefthalf[i]
+		i+=1
+		k+=1
+	while(j != len(righthalf)):
+		array[k] = righthalf[j]
+		j+=1
+		k+=1
+
+
+
+
+
+		
 	
 	#now we have arrays of 1 or 2 elements, so join and sort them
 
-	merge_together(start, middle, end)
+
 
 print('++++++')
 print('final')
+merge_sort(array)
+print(array)
--- python/random_generator.py
+++ python/random_generator.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3.5
 
+import sys
 import random
 
 f = open('test_data.txt', 'w')
+i = 30
+j = 40
+if(len(sys.argv) > 1):
+	i = int(sys.argv[1])	
+
+if(len(sys.argv) > 2):
+	j = int(sys.argv[2])
 
 #f.write('89\n87')
-for i in range(50):
-	f.write(str(random.randrange(30)) + '\n')
+for i in range(i):
+	f.write(str(random.randrange(j)) + '\n')
 
