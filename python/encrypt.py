13579e280060dbdb8edd670524181e6e2445b0b7
Krocodial <xxkraaklxx@gmail.com>
2017-03-20 18:00:13 -0700

    more progress on encrypter

---
+++ python/encrypt.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3.5
+
+import sys
+import os
+
+if(len(sys.argv) < 3):
+	print('not enough arguments')
+	sys.exit()
+try:
+	inf = open(sys.argv[1], 'r')
+except:
+	print('error opening file')
+	sys.exit()
+
+print(os.path.splitext(sys.argv[1])[0] + '.jpg')
+
+if(sys.argv[2] == 'encrypt'):
+	outf = open(os.path.splitext(sys.argv[1])[0] + '.encr', 'w+')
+	for line in inf:
+		outf.write(~line)
+
+
